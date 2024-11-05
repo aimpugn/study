164b23f27051c0794e97342ee1f569013b7f37c1
aimpugn <aimpugn@gmail.com>
2024-11-05 17:35:16 +0900

    jvm/examples/HowWorks/app/build.gradle.kts: 표준 입력 설정 추가
    기본적으로 gradle 작업은 사용자 입력을 필요로 하는 경우가 없기 때문에,
    입력이 빈 입력 스트림으로 설정되어 있습니다.
    
    터미널 통해 입력을 받기 위해 표준 입력을 설정합니다.

--- jvm/examples/HowWorks/app/build.gradle.kts
+++ jvm/examples/HowWorks/app/build.gradle.kts
@@ -94,5 +94,22 @@ application {
             }
             mainClass.set(targetMainClass)
         }
+
     }
+}
+
+/**
+ * Gradle은 주로 자동화된 빌드 도구로 사용되며, 빌드 작업은 대부분 사용자와의 상호작용 없이 실행됩니다.
+ * 따라서, 기본적으로 사용자 입력을 필요로 하지 않는 빈 입력 스트림이 기본값입니다.
+ * 이를 통해 빌드 프로세스가 중단되지 않고 자동으로 실행될 수 있도록 설계되었다고 합니다.
+ *
+ * 예를 들어, CI/CD 시스템이나 터미널 환경이 아닌 다른 방식으로 Gradle이 호출될 때 표준 입력이 필요 없는 경우가 있습니다.
+ *
+ * [readlnOrNull] 등을 사용하려면, 표준 입력이 명시적으로 설정해야 합니다.
+ *
+ * References:
+ * - https://docs.gradle.org/current/dsl/org.gradle.api.tasks.JavaExec.html#org.gradle.api.tasks.JavaExec:standardInput
+ */
+tasks.withType<JavaExec> {
+    standardInput = System.`in`
 }
