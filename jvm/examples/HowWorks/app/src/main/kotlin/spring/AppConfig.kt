cf686dd1d2c3640dc24c5d664f90d4e8f07749bb
aimpugn <aimpugn@gmail.com>
2025-01-06 22:47:44 +0900

    jvm/HowWorks/proxy/springaop: Spring AOP, Pointcut, Advice 등 테스트
    - org.aspectj:aspectjrt, org.springframework.boot:spring-boot-starter-aop 의존성 추가합니다.
    
    - 포인트컷, 어드바이스, 조인포인트 등을 사용하여 어떻게 스프링에서 AOP가 동작하는지 출력해봅니다.

---
+++ jvm/examples/HowWorks/app/src/main/kotlin/spring/AppConfig.kt
@@ -0,0 +1,12 @@
+package main.spring
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+// https://docs.spring.io/spring-boot/reference/features/aop.html#page-title
+// `AspectJ`가 클래스 패스에 있으면, 스프링 부트의 자동 구성은 AspectJ 자동 프록시를 활성화하므로,
+// `@EnableAspectJAutoProxy`를 추가하지 않아도 됩니다.
+// 단, 여기서는 구성 테스트 및 `exposeProxy` 설정을 추가하여 오버라이드하기 위해 직접 명시합니다.
+@EnableAspectJAutoProxy(exposeProxy = true)
+open class AppConfig
---
+++ jvm/examples/HowWorks/app/src/main/kotlin/spring/aop/LoggingBefore.kt
@@ -0,0 +1,5 @@
+package spring.aop
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoggingBefore
