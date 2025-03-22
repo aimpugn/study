cc0a2876c5e71f439bd5066ab8056b5447bb7d66
aimpugn <aimpugn@gmail.com>
2025-03-23 01:51:05 +0900

    books/clean_craftsmanship: 소프트웨어 장인 정신 이야기 p.147까지

---
+++ books/clean_craftsmanship/example/app/src/main/java/craftsmanship/f/SineTaylorCalculator_0.java
@@ -0,0 +1,35 @@
+package craftsmanship.f;
+
+public class SineTaylorCalculator_0 {
+    public double calculateTerm(double r, int n) {
+        int sign = calcSign(n - 1);
+        double power = calcPower(r, 2 * n - 1);
+        double factorial = calcFactorial(2 * n - 1);
+
+        return sign * power / factorial;
+    }
+
+    protected int calcSign(int n) {
+        int sign = 1;
+        for (int i = 0; i < n; i++) {
+            sign *= -1;
+        }
+        return sign;
+    }
+
+    protected double calcPower(double r, int n) {
+        double power = 1;
+        for (int i = 0; i < n; i++) {
+            power *= r;
+        }
+        return power;
+    }
+
+    protected double calcFactorial(int n) {
+        double fac = 1;
+        for (int i = 1; i <= n; i++) {
+            fac *= i;
+        }
+        return fac;
+    }
+}
---
+++ books/clean_craftsmanship/example/app/src/main/java/craftsmanship/f/SineTaylorCalculator_2.java
@@ -0,0 +1,50 @@
+package craftsmanship.f;
+
+public class SineTaylorCalculator_2 {
+    public double cos(double r) {
+        // 다음과 같은 항등식이 성립합니다.
+        return sin(r + Math.PI / 2);
+    }
+
+    public double sin(double r) {
+        double sin = 0;
+        // for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 20; i++) {
+            sin += calculateTerm(r, i);
+        }
+
+        return sin;
+    }
+
+    public double calculateTerm(double r, int n) {
+        int sign = calcSign(n - 1);
+        double power = calcPower(r, 2 * n - 1);
+        double factorial = calcFactorial(2 * n - 1);
+
+        return sign * power / factorial;
+    }
+
+    protected int calcSign(int n) {
+        int sign = 1;
+        for (int i = 0; i < n; i++) {
+            sign *= -1;
+        }
+        return sign;
+    }
+
+    protected double calcPower(double r, int n) {
+        double power = 1;
+        for (int i = 0; i < n; i++) {
+            power *= r;
+        }
+        return power;
+    }
+
+    protected double calcFactorial(int n) {
+        double fac = 1;
+        for (int i = 1; i <= n; i++) {
+            fac *= i;
+        }
+        return fac;
+    }
+}
