6b0aa25465a74211f644cd030f816e222731d8da
aimpugn <aimpugn@gmail.com>
2026-05-05 21:41:18 +0900

    chore: scaffold programmers java problem 468379

---
+++ algorithms/examples/programmers/jvm/src/main/java/p468379/Main.java
@@ -0,0 +1,25 @@
+package p468379;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/468379">선인장 숨기기</a>
+ */
+class Solution {
+    /**
+     * TODO: 문제 조건, 예제, 제약을 먼저 작은 문장으로 분해하세요.
+     */
+    public int[] solution(int m, int n, int h, int w, int[][] drops) {
+        // TODO: 풀이를 구현하세요.
+        return new int[]{};
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // TODO: Programmers 예제를 옮겨 적고 결과를 직접 확인하세요.
+        // System.out.println(Arrays.toString(solution.solution(...)));
+    }
+}
