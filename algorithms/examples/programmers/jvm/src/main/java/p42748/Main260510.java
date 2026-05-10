7800ee17645cffe8384dfe44925386f6ed1a5303
aimpugn <aimpugn@gmail.com>
2026-05-10 17:22:40 +0900

    Make 42748 scaffold copy paste ready

--- algorithms/examples/programmers/jvm/src/main/java/p42748/Main260510.java
+++ algorithms/examples/programmers/jvm/src/main/java/p42748/Main260510.java
@@ -5,6 +5,16 @@ import java.util.Arrays;
 /**
  * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java">K번째수</a>
  */
+class Solution {
+    /**
+     * TODO: 문제 조건, 예제, 제약을 먼저 작은 문장으로 분해하세요.
+     */
+    public int[] solution(int[] array, int[][] commands) {
+        // TODO: 풀이를 구현하세요.
+        return new int[]{};
+    }
+}
+
 public class Main260510 {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -16,16 +26,6 @@ public class Main260510 {
                         new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
                 )
         );
-    }
-
-    static class Solution {
-        /**
-         * TODO: 문제 조건, 예제, 제약을 먼저 작은 문장으로 분해하세요.
-         */
-        public int[] solution(int[] array, int[][] commands) {
-            // TODO: 풀이를 구현하세요.
-            return new int[]{};
-        }
     }
 
     private static void assertArrayEquals(int[] expected, int[] actual) {
