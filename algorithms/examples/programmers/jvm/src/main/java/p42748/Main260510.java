7908e7ee242bdfad42466c12a7d74f65dd500ca1
aimpugn <aimpugn@gmail.com>
2026-05-10 16:44:19 +0900

    Add Programmers 42748 Java scaffold

---
+++ algorithms/examples/programmers/jvm/src/main/java/p42748/Main260510.java
@@ -0,0 +1,37 @@
+package p42748;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java">K번째수</a>
+ */
+public class Main260510 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        assertArrayEquals(
+                new int[]{5, 6, 3},
+                solution.solution(
+                        new int[]{1, 5, 2, 6, 3, 7, 4},
+                        new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
+                )
+        );
+    }
+
+    static class Solution {
+        /**
+         * TODO: 문제 조건, 예제, 제약을 먼저 작은 문장으로 분해하세요.
+         */
+        public int[] solution(int[] array, int[][] commands) {
+            // TODO: 풀이를 구현하세요.
+            return new int[]{};
+        }
+    }
+
+    private static void assertArrayEquals(int[] expected, int[] actual) {
+        if (!Arrays.equals(expected, actual)) {
+            throw new AssertionError("expected=" + Arrays.toString(expected)
+                    + ", actual=" + Arrays.toString(actual));
+        }
+    }
+}
