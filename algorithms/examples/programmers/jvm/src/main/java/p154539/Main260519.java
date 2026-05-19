e06b93c0ab90350ac9f2a20a66845c0d39e8e8eb
aimpugn <aimpugn@gmail.com>
2026-05-19 21:01:46 +0900

    Use TestCase harness for 154539 practice

--- algorithms/examples/programmers/jvm/src/main/java/p154539/Main260519.java
+++ algorithms/examples/programmers/jvm/src/main/java/p154539/Main260519.java
@@ -1,6 +1,10 @@
 package p154539;
 
+import support.TestCase;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/154539">뒤에 있는 큰 수 찾기</a>
@@ -36,14 +40,52 @@ class Solution {
 
 public class Main260519 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        var solution = new Solution();
 
-        // 공식 예제:
-        // numbers=[2, 3, 3, 5] -> [3, 5, 5, -1]
-        // numbers=[9, 1, 5, 3, 6, 2] -> [-1, 5, 6, 6, -1, -1]
-        //
-        // 구현한 뒤에는 아래처럼 눈으로 먼저 확인하고, 필요하면 AssertionError 형태로 바꿔 보세요.
-        // System.out.println(Arrays.toString(solution.solution(new int[]{2, 3, 3, 5})));
-        // System.out.println(Arrays.toString(solution.solution(new int[]{9, 1, 5, 3, 6, 2})));
+        var testCases = List.of(
+            new TestCase<>(new int[]{2, 3, 3, 5}, new int[]{3, 5, 5, -1}),
+            new TestCase<>(new int[]{9, 1, 5, 3, 6, 2}, new int[]{-1, 5, 6, 6, -1, -1})
+        );
+
+        for (var testCase : testCases) {
+            var result = solution.solution(testCase.input());
+            assertEquals(testCase, result);
+        }
+    }
+
+    private static void assertEquals(TestCase<?, ?> testCase, Object actual) {
+        if (!valuesEqual(testCase.answer(), actual)) {
+            throw new AssertionError(
+                "input=" + valueToString(testCase.input())
+                    + ", expected=" + valueToString(testCase.answer())
+                    + ", actual=" + valueToString(actual)
+            );
+        }
+    }
+
+    private static boolean valuesEqual(Object expected, Object actual) {
+        if (expected instanceof int[] expectedArray && actual instanceof int[] actualArray) {
+            return Arrays.equals(expectedArray, actualArray);
+        }
+        if (expected instanceof long[] expectedArray && actual instanceof long[] actualArray) {
+            return Arrays.equals(expectedArray, actualArray);
+        }
+        if (expected instanceof Object[] expectedArray && actual instanceof Object[] actualArray) {
+            return Arrays.deepEquals(expectedArray, actualArray);
+        }
+        return Objects.equals(expected, actual);
+    }
+
+    private static String valueToString(Object value) {
+        if (value instanceof int[] values) {
+            return Arrays.toString(values);
+        }
+        if (value instanceof long[] values) {
+            return Arrays.toString(values);
+        }
+        if (value instanceof Object[] values) {
+            return Arrays.deepToString(values);
+        }
+        return String.valueOf(value);
     }
 }
